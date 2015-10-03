d4b24153e5efb281c1550a52711c38db1e8ca6c4
ppam <ppam445@gmail.com>
2015-10-03 12:58:08 -0400

    basic scaffolding for side bets

--- app/administration/PPamScratch.scala
+++ app/administration/PPamScratch.scala
@@ -1,8 +1,12 @@
 package administration
 
+import domain.Week
+
 /**
  * Created by Praveen on 10/3/2015.
  */
-class PPamScratch {
-//new comment
+object PPamScratch extends App{
+
+  //val allPicks = Week.allWeeks.map(x=>DataGenerator.getDefaultPicks())
+
 }
